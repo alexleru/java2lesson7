3e513d475a072982284818b11b321402239476f0
alexleru <alexleru@yandex.ru>
2019-01-23 22:53:34 +0300

    lesson7

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -3,7 +3,7 @@ package lesson7;
 import client.UIWindows;
 
 public class Main {
-
+//для запуска клиента. Запускаем после MainServer
     public static void main(String[] args) {
         new UIWindows();
     }
--- src/lesson7/MainServer.java
+++ src/lesson7/MainServer.java
@@ -3,6 +3,7 @@ package lesson7;
 import server.MyServer;
 
 public class MainServer {
+    //для запуска сервера. Запускаем самым первым
     public static void main(String[] args) {
         new MyServer();
     }
