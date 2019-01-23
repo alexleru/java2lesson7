03d975f0b8ed10d75e343c9ed29eb15a0fa95784
alexleru <alexleru@yandex.ru>
2019-01-23 22:43:56 +0300

    initial commit

---
+++ src/lesson7/Main.java
@@ -0,0 +1,10 @@
+package lesson7;
+
+import client.UIWindows;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new UIWindows();
+    }
+}
---
+++ src/lesson7/MainServer.java
@@ -0,0 +1,9 @@
+package lesson7;
+
+import server.MyServer;
+
+public class MainServer {
+    public static void main(String[] args) {
+        new MyServer();
+    }
+}
